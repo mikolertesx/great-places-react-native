b18824a086e545c698ae5c2866d4cd80b4a338ec
Mikolertesx <mgangsal@outlook.com>
2020-12-04 14:38:22 -0600

    Configuring camera access

---
+++ components/ImagePicker.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { View, Button, Image, Text, StyleSheet, Alert } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+import Colors from "../constants/Colors";
+
+const noPermissionAlert = () => {
+  Alert.alert(
+    "Insufficient permissions!",
+    "You need to grant camera permissions to use this app.",
+    [{ text: "Okay" }]
+  );
+};
+
+const ImgPicker = (props) => {
+  const [pickedImage, setPickedImage] = useState();
+
+  const verifyPermissions = async () => {
+    let result;
+    result = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    result = result && (await Permissions.askAsync(Permissions.CAMERA));
+    if (result.status !== "granted") {
+      noPermissionAlert();
+      return false;
+    }
+    return true;
+  };
+
+  const takeImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      noPermissionAlert();
+      return;
+    }
+    const image = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+
+    setPickedImage(image.uri);
+  };
+
+  return (
+    <View style={styles.imagePicker}>
+      <View style={styles.imagePreview}>
+        {pickedImage ? (
+          <Image style={styles.image} source={{ uri: pickedImage }} />
+        ) : (
+          <Text>No image picked yet.</Text>
+        )}
+      </View>
+      <Button
+        title="Take Image"
+        color={Colors.primary}
+        onPress={takeImageHandler}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imagePicker: {
+    alignItems: "center",
+  },
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#ccc",
+    borderWidth: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ImgPicker;
