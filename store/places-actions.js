9b8c43e96ae3446989afcd6994dc02044ca7f3e1
Mikolertesx <mgangsal@outlook.com>
2020-12-05 19:06:51 -0600

    CRU of CRUD ready

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,6 +1,8 @@
 import * as FileSystem from "expo-file-system";
 
 export const ADD_PLACE = "ADD_PLACE";
+export const SET_PLACES = "SET_PLACES";
+import { insertPlace, fetchPlaces } from "../helpers/db";
 
 export const addPlace = (title, image) => {
   return async (dispatch) => {
@@ -12,14 +14,31 @@ export const addPlace = (title, image) => {
         from: image,
         to: newPath,
       });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        "dummy address",
+        15.6,
+        12.3
+      );
+      return dispatch({
+        type: ADD_PLACE,
+        placeData: { id: dbResult.insertId, title: title, image: newPath },
+      });
     } catch (err) {
       console.log(err);
       throw err;
     }
+  };
+};
 
-    return dispatch({
-      type: ADD_PLACE,
-      placeData: { title: title, image: newPath },
-    });
+export const loadPlaces = () => {
+  return async (dispatch) => {
+    try {
+      const dbResult = await fetchPlaces();
+      dispatch({ type: SET_PLACES, places: dbResult.rows._array });
+    } catch (err) {
+      throw err;
+    }
   };
 };
