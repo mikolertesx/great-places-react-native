5faee3b096b4e74e947676a5a59a15ea50d3e24a
Mikolertesx <mgangsal@outlook.com>
2020-12-08 15:25:46 -0600

    Displaying an interactive map

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Image, View, StyleSheet } from "react-native";
+import { Image, View, TouchableOpacity, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 import ENV from "../env";
 
@@ -10,14 +11,34 @@ const MapPreview = (props) => {
     imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${ENV.googleApiKey}`;
   }
 
+  /* 
+    I can't use billing for the google api.
+    So I made it optional, and have a native map instead.
+    Switching between the two should be seamless if I ever get the money.
+  */
   return (
-    <View style={{ ...styles.mapPreview, ...props.style }}>
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={{ ...styles.mapPreview, ...props.style }}
+    >
       {props.location ? (
-        <Image style={styles.mapImage} source={{ uri: imagePreviewUrl }} />
+        props.useStaticMap ? (
+          <Image style={styles.mapImage} source={{ uri: imagePreviewUrl }} />
+        ) : (
+          <MapView
+            region={{
+              latitude: props.location.lat,
+              longitude: props.location.lng,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            style={styles.mapImage}
+          />
+        )
       ) : (
         props.children
       )}
-    </View>
+    </TouchableOpacity>
   );
 };
 
